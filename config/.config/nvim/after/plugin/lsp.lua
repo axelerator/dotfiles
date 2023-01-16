9609b9715defab7e4322b5b83586f7156af9d7d7
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2023-01-16 09:32:11 -0500

    <leader>f to format buffer

--- config/.config/nvim/after/plugin/lsp.lua
+++ config/.config/nvim/after/plugin/lsp.lua
@@ -54,6 +54,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
 end)
 
 lsp.setup()
