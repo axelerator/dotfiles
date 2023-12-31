89fb4df7f6637c1eb710109395ff33ab0ad2e71e
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2023-12-31 10:29:57 -0500

    nvim revamp

--- config/.config/nvim/after/plugin/colors.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd.colorscheme("palenight")
-vim.cmd("let g:palenight_terminal_italics=1")
-vim.cmd("let g:airline_theme = \"palenight\"")
--- config/.config/nvim/lua/axelerator/init.lua
+++
@@ -1,3 +0,0 @@
-require("axelerator.remap")
-require("axelerator.set")
-
